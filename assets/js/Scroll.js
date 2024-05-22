8902a1f0b8db7c7b7e28f4eb67455e04d5250310
novruzoff9 <yagmurnov2005@gmail.com>
2024-05-22 12:04:44 +0400

    Added Welcome Section

--- assets/js/Scroll.js
+++ assets/js/Scroll.js
@@ -1,22 +1,19 @@
-$(function() {
-    $(window).on('scroll', function() {
-        // //Header Scroll
-        // if ($(this).scrollTop() > 50) {
-        //     $('header').addClass('scrolled');
-        // } else {
-        //     $('header').removeClass('scrolled');
-        // }
-        var windowHeight = $(window).height();
-        var scrollDistance = $(window).scrollTop();
+$(function () {
+  $(window).on("scroll", function () {
+    // //Header Scroll
+    // if ($(this).scrollTop() > 50) {
+    //     $('header').addClass('scrolled');
+    // } else {
+    //     $('header').removeClass('scrolled');
+    // }
+    var windowHeight = $(window).height();
+    var scrollDistance = $(window).scrollTop();
+    var elementTop = $(".advantage").offset().top;
 
-        $('.advantage').each(function() {
-            var elementTop = $(this).offset().top;
-
-            if (scrollDistance + windowHeight - 150 >= elementTop) {
-                $(this).addClass('show');
-            } else {
-                $(this).removeClass('show');
-            }
-        });
-    });
+    if (scrollDistance + windowHeight - 150 >= elementTop) {
+      $(".advantage").addClass("show");
+    } else {
+      $(".advantage").removeClass("show");
+    }
+  });
 });

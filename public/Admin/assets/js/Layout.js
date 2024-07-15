ffce8097561cdff536213df16a546ef8d1dbf2e4
novruzoff9 <yagmurnov2005@gmail.com>
2024-07-15 23:08:06 +0400

    Fixed Admin panel

--- public/Admin/assets/js/Layout.js
+++ public/Admin/assets/js/Layout.js
@@ -1,11 +1,38 @@
-$('#menubtn').click(function () {
+$('#menubtn').click(toggleSideBar);
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "b") {
+    event.preventDefault(); // Varsayılan tarayıcı kısayolunu engelle
+    toggleSideBar();
+  }
+});
+
+function toggleSideBar(){
   $('aside').toggleClass('mini-show');
   $('header').toggleClass('show-more');
   $('#content').toggleClass('show-more');
-});
+}
 $('#notficiationsbtn').click(function () {
+  $("#profile-menu").hide();
   $('#notficiations-menu').toggle();
 });
-$('#user-data').click(function () {
-  $('#profile-menu').toggle();
+
+$("#user-data").click(showProfile);
+document.addEventListener("keydown", function(event){
+  if(event.ctrlKey && event.key === "p"){
+    event.preventDefault();
+    showProfile();
+  }
+});
+
+function showProfile(){
+  $("#notficiations-menu").hide();
+  $("#profile-menu").toggle();
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "/") {
+    event.preventDefault(); // Bu, `/` tuşuna basıldığında varsayılan davranışı (örn. arama yapma) engeller.
+    document.getElementById("fullsearch").focus();
+  }
 });
